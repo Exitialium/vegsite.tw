bad08f1f898e7aee5433e0bff152fb42c020499a
Exitialium <henrywu0424@gmail.com>
2020-08-13 22:10:25 +0800

    Info

--- info/views.py
+++ info/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import Restaurants
 # Create your views here.
 
-def home(request):
-    rest = Restaurants.objects.all()
-    return render(request, 'restaurant.html', {'restaurants': rest})
+class RestList(generic.ListView):
+    queryset = Restaurants.objects.order_by('rank')
+    template_name = 'index.html'
+
+class RestDetail(generic.DetailView):
+    model = Restaurants
+    template_name = 'restaurants.html'
 
