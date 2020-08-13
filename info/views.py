3982c862d38966a7f19da56a3d74ca60111aeffe
Exitialium <henrywu0424@gmail.com>
2020-08-13 23:11:52 +0800

    Fix

--- info/views.py
+++ info/views.py
@@ -12,13 +12,5 @@ class RestDetail(generic.DetailView):
     queryset = Restaurants.order_by('rank')
     template_name = 'index.html'
 
-class PostDetail(generic.DetailView):
-    model = Restaurants
-    template_name = 'restaurant.html'
 
-
-
-def home(request):
-    rest = Restaurants.objects.all()
-    return render(request, 'restaurant.html', {'restaurants': rest})
 
