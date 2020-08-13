16e06c6673a924f216d75a524f4c6d8487f05a8e
Exitialium <henrywu0424@gmail.com>
2020-08-14 07:11:57 +0800

    fix #3

--- info/models.py
+++ info/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Restaurants(models.Model):
     name = models.CharField(max_length=100,unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.CharField(max_length=500)
     dirName = models.CharField(max_length=100,unique=True)
     address = models.CharField(max_length=1000,unique=True)
