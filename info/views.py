35edf5a0d04fcf670c25951eb6d39060ab02c946
Exitialium <henrywu0424@gmail.com>
2020-08-14 06:32:51 +0800

    Django Compatibility

--- info/views.py
+++ info/views.py
@@ -4,14 +4,12 @@ from django.views import generic
 # Create your views here.
 
 class RestList(generic.ListView):
-    queryset = Restaurants.objects.order_by('rank')
+    queryset = Restaurants
     template_name = 'index.html'
 
 class RestDetail(generic.DetailView):
     model = Restaurants
     template_name = 'restaurants.html'
-    queryset = Restaurants.order_by('rank')
-    template_name = 'index.html'
 
 
 
