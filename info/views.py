f5c7d97aa995a611254acbfa8987a92a1963428b
Exitialium <henrywu0424@gmail.com>
2020-08-13 21:25:56 +0800

    Model class update

--- info/views.py
+++ info/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from .models import Restaurants
 # Create your views here.
 
+class RestList(generic.ListView):
+    queryset = Restaurants.order_by('rank')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Restaurants
+    template_name = 'restaurant.html'
+
+
+
 def home(request):
     rest = Restaurants.objects.all()
     return render(request, 'restaurant.html', {'restaurants': rest})
