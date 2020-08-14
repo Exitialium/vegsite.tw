f037c14f83721fce440bea3a81d67777b7516d17
Exitialium <henrywu0424@gmail.com>
2020-08-14 18:13:52 +0800

    Polishing + Slideshow

--- info/models.py
+++ info/models.py
@@ -6,7 +6,7 @@ import uuid
 class Restaurants(models.Model):
     name = models.CharField(max_length=100,unique=True)
     slug = models.SlugField(max_length=200, default=uuid.uuid4, unique=True)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=10000)
     dirName = models.CharField(max_length=100,unique=True)
     address = models.CharField(max_length=1000,unique=True)
     mapUrl = models.CharField(max_length=10000,unique=True)
