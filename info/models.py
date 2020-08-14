6089c6bae883929ca3a8fbdeed771c8c7ad3602b
Exitialium <henrywu0424@gmail.com>
2020-08-14 09:56:11 +0800

    Slug default fix #2

--- info/models.py
+++ info/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 # Create your models here.
 
 class Restaurants(models.Model):
     name = models.CharField(max_length=100,unique=True)
-    slug = models.SlugField(max_length=200, default='0000000', unique=True)
+    slug = models.SlugField(max_length=200, default=uuid.uuid4, unique=True)
     description = models.CharField(max_length=500)
     dirName = models.CharField(max_length=100,unique=True)
     address = models.CharField(max_length=1000,unique=True)
